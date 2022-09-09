bd0e3adccc2aed23995e89b07e03c11aa111bcfa
SSDmitiry <XYZmail@yandex.ru>
2022-09-09 22:20:22 +0400

    Add project files.

---
+++ Metanit_03/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string firstname = "Имя";
+string lastname = "фамилия";
+string cons = Console.ReadLine();
+Console.WriteLine(firstname + lastname + cons);  
+
