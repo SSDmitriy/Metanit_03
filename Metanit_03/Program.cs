f762b1654eca02333274a169f1f467ae3a9fe748
SSDmitiry <XYZmail@yandex.ru>
2022-09-13 22:00:13 +0400

    комит в мастер

--- Metanit_03/Program.cs
+++ Metanit_03/Program.cs
@@ -1,9 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string firstname = "Имя";
-string lastname = "фамилия";
+string firstname = "Имечко";
+string lastname = "Фамилия";
 string cons = Console.ReadLine();
 Console.WriteLine(firstname + " " + lastname + " " + cons);
 
 //add one more line
-Console.WriteLine("commit two");
