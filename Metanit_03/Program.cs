34ece2877ba7a5b6263b93eaadd60d85bd2cd9b1
SSDmitiry <XYZmail@yandex.ru>
2022-09-13 21:52:33 +0400

    data types

--- Metanit_03/Program.cs
+++ Metanit_03/Program.cs
@@ -3,7 +3,17 @@ Console.WriteLine("Hello, World!");
 string firstname = "Имя";
 string lastname = "фамилия";
 string cons = Console.ReadLine();
-Console.WriteLine(firstname + " " + lastname + " " + cons);
+Console.WriteLine(firstname + " \t" + lastname + "\n" + cons);
 
 //add one more line
 Console.WriteLine("commit two");
+
+int age = 33;
+ulong distanse = 3;
+float density = 2.7f;
+double G = 0.0000000000667;
+
+Console.WriteLine($"Возраст: {age}");
+Console.WriteLine($"расстояние: {distanse}");
+Console.WriteLine($"Плотность: {density}");
+Console.WriteLine($"Грав.постоянная: {G}");
