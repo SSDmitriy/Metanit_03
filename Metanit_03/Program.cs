d23eb822e71befbbbc8e035cdb074cfc01dc92c6
SSDmitiry <XYZmail@yandex.ru>
2022-09-20 19:58:33 +0400

    add arrays

--- Metanit_03/Program.cs
+++ Metanit_03/Program.cs
@@ -56,16 +56,98 @@ Console.WriteLine("Инвертированный Х = " + y + " = " + (~y));
 int x2 = 4;
 Console.WriteLine($"x2 = {x2}. Сдвинуть на 4 разряда влево: X2 << 4 = {x2 << 2}");
 
-byte a = 4;
-byte b = (byte)(a + 1);
+byte ab = 4;
+byte bb = (byte)(ab + 1);
 
 //явное приведение
 double d3 = 14.0;
 decimal dc3 = (decimal)d3;
 Console.WriteLine("Decimal dc3 = {0}", dc3);
 
+//Console.WriteLine("Введите два числа");
+//int num1, num2;
+//num1 = Convert.ToInt32(Console.ReadLine());
+//num2 = Convert.ToInt32(Console.ReadLine());
+//string? result = null;
+
+//if (num1 > num2)
+//{
+//    result = "больше";
+//}
+//else if (num1 < num2)
+//{
+//    result = "меньше";
+//}
 
 
+//Console.WriteLine($"num1 " + result + ", чем num2");
+
+//циклы
+Console.WriteLine("Циклы");
+//for (int i = 1; i < 5; i++)
+//{
+//    Console.Write($"{i}, ");
+//}
+
+//int j = 6;
+//for (; j < 10;)
+//{
+//    Console.Write($"{j}, ");
+//    j++;
+
+//}
+
+
+int a, b;
+a = b = 1;
+for (; a < 11; a++)
+{
+
+   
+    for (; b < 11; b++)
+    {
+        Console.Write((a * b) + ", ");
+    }
+
+    b = a;
+    Console.WriteLine();
+}
+
+
+//for (int i = 0; i < 100; i++)
+//{
+//    Console.WriteLine(i);
+//    if (i > 10 && i < 20)
+//    {
+//        i += 5;
+//    }
+
+//    if (i > 50)
+//    {
+//        i = 99;
+//    }
+//}
+
+//Массивы
+int[] nums = new int[5];
+
+for(int i = 0; i < nums.Length; i++)
+{
+    nums[i] = i+5;
+}
+
+
+foreach (int xit in nums)
+{
+    Console.WriteLine(xit + ", ");
+    
+}
+
+Console.WriteLine("Последний и предпоследний элементы");
+//длина массива - свойство .Lenght
+//последний, предпоследний элемент - операторы [^1], [^2]
+Console.WriteLine(nums[^1]);
+Console.WriteLine(nums[^2]);
 
 
 
